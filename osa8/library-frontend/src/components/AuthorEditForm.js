82441a5061c4f9b3419d3ac7f571b54429fa66a8
sri <siiri.saaksvuori@gmail.com>
2022-08-18 14:34:28 +0300

    osa8: 8.8-8.12

---
+++ osa8/library-frontend/src/components/AuthorEditForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import Select from 'react-select';
+import { EDIT_AUTHOR} from '../queries'
+
+const AuthorEditForm = ({authors}) => {
+
+    const [year, setYear] = useState('')
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [ changeYear ] = useMutation(EDIT_AUTHOR)
+
+    const submit = async (event) => {
+        event.preventDefault()
+        changeYear({variables: {name: selectedOption.value, setBornTo: parseInt(year)}})
+        setYear('')
+    }
+
+   
+    const options = authors.map( ({name}) => {
+        const str = `{"value": "${name}", "label": "${name}"}`
+        return JSON.parse(str)
+    })
+
+
+    
+    return (
+        <div>
+            <h2>set the year of birth for author</h2>
+            <form onSubmit={submit}>
+                <Select
+                    defaultValue={selectedOption}
+                    onChange={setSelectedOption}
+                    options={options}
+                />
+                born <input
+                value={year}
+                onChange={({ target }) => setYear(target.value)}
+            />
+                <button type='submit'>update</button>
+            </form>
+            
+        </div>
+    )
+}
+
+export default AuthorEditForm
